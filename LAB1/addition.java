dbcfdfeb7400a63899533d956fdfd4ab2b90af1f
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2020-02-08 20:05:44 +0530

    Create addition.java

---
+++ LAB1/addition.java
@@ -0,0 +1,32 @@
+package com.example.addition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText et1 = (EditText) findViewById(R.id.editText1);
+        final EditText et2 = (EditText) findViewById(R.id.editText2);
+        final Button bt = (Button) findViewById(R.id.button);
+        final TextView tv = (TextView) findViewById(R.id.textView);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i1 = Integer.parseInt(et1.getText().toString());
+                int i2 = Integer.parseInt(et2.getText().toString());
+                int sum = i1 + i2;
+                tv.setText("The sum of "+i1+" and "+i2+"is "+sum);
+            }
+        });
+    }
+}
