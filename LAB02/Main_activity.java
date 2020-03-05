f0660b476be0e3fbe0708bf825f95c7ede58083d
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2020-03-05 20:43:01 +0530

    Update Main_activity.java

--- LAB02/Main_activity.java
+++ LAB02/Main_activity.java
@@ -2,28 +2,47 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class second_activity extends AppCompatActivity {
-    TextView t1,t2,t3;
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2;
+    Button bt;
+    Spinner s;
+    String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
     String name,reg,dept;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_activity);
-        t1=(TextView)findViewById(R.id.textView);
-        t2=(TextView)findViewById(R.id.textView1);
-        t3=(TextView)findViewById(R.id.textView2);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.editText);
+        e2=(EditText)findViewById(R.id.editText1);
+        bt=(Button)findViewById(R.id.button);
+        s=(Spinner)findViewById(R.id.spinner);
 
-        Intent i = getIntent();
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
+        s.setAdapter(adapter);
 
-        name=i.getStringExtra("name_key");
-        reg=i.getStringExtra("reg_key");
-        dept=i.getStringExtra("dept_key");
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v) {
+                name=e1.getText().toString();
+                reg=e2.getText().toString();
+                dept=s.getSelectedItem().toString();
 
-        t1.setText(name);
-        t2.setText(reg);
-        t3.setText(dept);
+                Intent i = new Intent(MainActivity.this,second_activity.class);
+
+                i.putExtra("name_key",name);
+                i.putExtra("reg_key",reg);
+                i.putExtra("dept_key",dept);
+
+                startActivity(i);
+            }
+        });
     }
 }
